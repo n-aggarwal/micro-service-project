b592ee99464dc585a8fefd9d74b9313dd53d1a92
Nishant Aggarwal <121606501+n-aggarwal@users.noreply.github.com>
2024-01-02 18:34:27 -0500

    implement synchronous commnication between order and inventory service

--- inventory-service/src/main/java/com/nishant/inventoryservice/repositories/InventoryRepository.java
+++ inventory-service/src/main/java/com/nishant/inventoryservice/repositories/InventoryRepository.java
@@ -3,8 +3,11 @@ package com.nishant.inventoryservice.repositories;
 import com.nishant.inventoryservice.domains.entites.InventoryEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface InventoryRepository extends JpaRepository<InventoryEntity, Long> {
     Optional<InventoryEntity> findBySkuCode(String skuCode);
+
+    List<InventoryEntity> findBySkuCodeIn(List<String> skuCode);
 }
--- order-service/src/main/java/com/nishant/orderservice/services/impl/OrderServiceImpl.java
+++ order-service/src/main/java/com/nishant/orderservice/services/impl/OrderServiceImpl.java
@@ -1,5 +1,6 @@
 package com.nishant.orderservice.services.impl;
 
+import com.nishant.orderservice.domains.dto.InventoryResponse;
 import com.nishant.orderservice.domains.dto.OrderLineItemsDto;
 import com.nishant.orderservice.domains.dto.OrderRequest;
 import com.nishant.orderservice.domains.entites.OrderEntity;
@@ -7,11 +8,16 @@ import com.nishant.orderservice.domains.entites.OrderLineItemsEntity;
 import com.nishant.orderservice.mappers.Mapper;
 import com.nishant.orderservice.repositories.OrderRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.reactive.function.client.WebClient;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
+
+import static java.util.Arrays.stream;
 
 @Service
 @RequiredArgsConstructor
@@ -20,6 +26,7 @@ public class OrderServiceImpl implements com.nishant.orderservice.services.Order
 
     private final Mapper<OrderLineItemsEntity, OrderLineItemsDto> orderLineItemsMapper;
     private final OrderRepository orderRepository;
+    private final WebClient webClient;
 
     @Override
     public void placeOrder(OrderRequest orderRequest) {
@@ -30,12 +37,46 @@ public class OrderServiceImpl implements com.nishant.orderservice.services.Order
         List<OrderLineItemsDto> orderRequestItemListDto = orderRequest.getOrderLineItemsDtoList();
         List<OrderLineItemsEntity> orderLineItemsEntityList = orderRequestItemListDto
                 .stream()
-                .map(orderLineItemsDto -> orderLineItemsMapper.mapFromDto(orderLineItemsDto))
+                .map(orderLineItemsMapper::mapFromDto)
                 .toList();
 
         order.setOrderLineItemsEntityList(orderLineItemsEntityList);
 
-        orderRepository.save(order);
+        List<String> skuCodesList = orderLineItemsEntityList
+                .stream()
+                .map(OrderLineItemsEntity::getSkuCode)
+                .toList();
+
+        /*
+         * Call Inventory Service, and place order if product is in stock
+         */
+        InventoryResponse[] inventoryResponseArray;
+        inventoryResponseArray = webClient.get()
+                .uri("http://localhost:8083/api/inventory",
+                        uriBuilder -> uriBuilder.queryParam(
+                                "skuCode", skuCodesList).build()
+                )
+                .retrieve()
+                .bodyToMono(InventoryResponse[].class)
+                .block();
+
+        if (inventoryResponseArray == null) {
+            throw new IllegalArgumentException("The Entered Sku-codes are not valid.");
+        }
+
+        boolean allProductsInStock = Arrays
+                                        .stream(inventoryResponseArray)
+                                        .allMatch(InventoryResponse::isInStock);
+
+        if (allProductsInStock) {
+            orderRepository.save(order);
+        }
+        else {
+            throw new IllegalArgumentException("Product is not in stock, " +
+                    "please try again later");
+        }
+
+
 
     }
 }
